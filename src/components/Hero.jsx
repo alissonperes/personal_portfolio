dd4678d020d67e0e2529e51874ef7eb4b79c292e
Alisson Peres <aperes.dev@gmail.com>
2025-03-08 04:35:14 -0300

    mobile fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = ({ heroTitle, position, userIntro, github, linkedin, heroImg }) => 
   return (
     <div className='bg-violet-200 py-10 shadow-md shadow-violet-200 md:shadow-violet-300/60 md:shadow-2xl' id='home'>
       <div className='align-element flex justify-center items-center md:max-h-[60vh]'>
-        <article className='md:max-w-[28rem]'>
+        <div className='md:max-w-[28rem]'>
           <h1 className='text-2xl font-bold'>{heroTitle}</h1>
           <p className='mt-4 text-xl text-slate-700 capitalize'>
             {position}
@@ -28,9 +28,9 @@ const Hero = ({ heroTitle, position, userIntro, github, linkedin, heroImg }) => 
               {linkedin.icon}
             </a>
           </div>
-        </article>
+        </div>
         <div className='hidden md:block '>
-        <img src={heroImg} className='size-80' />
+        <img src={heroImg} className='size-80 max-h-[67vh]' />
         </div>
       </div>
     </div>
