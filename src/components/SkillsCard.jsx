902e0e945968986dc4d7daaf1685030f766f9cb5
Alisson Peres <aperes.dev@gmail.com>
2025-03-08 23:23:24 -0300

    new icons

--- src/components/SkillsCard.jsx
+++ src/components/SkillsCard.jsx
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 const SkillsCard = ({ title, text, iteration, icons }) => {
   return (
     <article className='rounded-xl bg-white p-8 text-center shadow-md hover:shadow-lg hover:shadow-violet-700 delay-150 duration-700 transition cursor-pointer'>
-      <div className='grid -translate-y-10 transform items-center justify-center '>
-        <img src={icons[iteration%icons.length]} />
+      <div className='grid -translate-y-10 transform items-center justify-center'>
+        <img className='rotate-[3.142rad]' src={icons[iteration%icons.length]} />
       </div>
       <div className='-mt-8'>
         <h6 className='font-bold '>{title}</h6>
