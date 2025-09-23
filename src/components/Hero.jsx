c723983a7028de31225adaad7f1d7f88333844a5
Alisson Peres <aperes.dev@gmail.com>
2025-09-22 23:21:58 -0300

    update hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,36 +2,44 @@ import PropTypes from 'prop-types';
 
 const Hero = ({ heroTitle, position, userIntro, github, linkedin, heroImg }) => {
   return (
-    <section id="home" className='container-fluid d-md-flex align-items-center justify-content-center'>
-      <img src={heroImg} className='col-md-6 col-xl-5 img-fluid' />
-      <div className='col-md-6 col-xl-4 m-5 p-3 text-bg-info rounded'>
-        <h2 className=''>{heroTitle}</h2>
-        <p className=''>
-          {position}
-        </p>
-        <p className=''>
-          {userIntro}
-        </p>
-        <div className='fs-1'>
-          <a
-            href={github.link}
-            target='_blank'
-            rel='noreferrer'
-            className='text-dark'
-          >
-            {github.icon}
-          </a>
-          <a
-            href={linkedin.link}
-            target='_blank'
-            rel='noreferrer'
-            className='text-dark'
-          >
-            {linkedin.icon}
-          </a>
+    <>
+    <section id="home" className='container'>
+      <div className='row h-100 align-items-center justify-content-center'>
+        <div className="card mb-3">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img src={heroImg} className="img-fluid rounded-start" alt="..." />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">{heroTitle}</h5>
+                <p className="card-text"><small className="text-body-secondary">{position}</small></p>
+                <p className="card-text">{userIntro}</p>
+                <div className='fs-1'>
+                  <a
+                    href={github.link}
+                    target='_blank'
+                    rel='noreferrer'
+                    className='text-info'
+                  >
+                    {github.icon}
+                  </a>
+                  <a
+                    href={linkedin.link}
+                    target='_blank'
+                    rel='noreferrer'
+                    className='text-info'
+                  >
+                    {linkedin.icon}
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
+    </>
   );
 };
 
